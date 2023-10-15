53cedc48eb1d73335ccc63583aacc792ec72fa05
Pratham Kamble <prathamkamble727@gmail.com>
2023-10-15 18:32:38 +0530

    done

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-// const navbar = document.querySelector(".navbar");
-// const mainContainer = document.querySelector(".main-container");
+const navbar = document.querySelector(".navbar");
+const mainContainer = document.querySelector(".main-container");
 
-// mainContainer.addEventListener("scroll", () => {
+mainContainer.addEventListener("scroll", () => {
 
-//     if (mainContainer.scrollTop > 0) {
-//         navbar.style.backgroundColor = "#121212"; // Change the color as desired
-//     }
-//     else {
-//         navbar.style.backgroundColor = "transparent";
-//     }
-// });
+    if (mainContainer.scrollTop > 0) {
+        navbar.style.backgroundColor = "#121212"; // Change the color as desired
+    }
+    else {
+        navbar.style.backgroundColor = "transparent";
+    }
+});
 
 let songIdx = 0;
 let audioElem = new Audio('./1.mp3')
