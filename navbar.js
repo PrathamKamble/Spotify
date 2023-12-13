f0816eec045b2a4eb2c651b9520b09d34b483f76
Pratham Kamble <prathamkamble727@gmail.com>
2023-12-14 00:27:44 +0530

    alert modal added and project done

--- navbar.js
+++ navbar.js
@@ -1,3 +1,25 @@
+// alert("Welcome User🙋‍♂️!\nI have listed 20 songs in this replica.\nPlease click on the 'Liked Songs' playlist to enjoy the added songs & have full control over music 🎶");
+
+const modal = document.getElementById('popup-modal');
+const container = document.querySelector('.my-container'); 
+const close_modal = document.getElementById('close-btn'); 
+
+window.onload = () => {
+    container.style.opacity = "0.2";
+    modal.style.top = "20%";
+    
+    
+    close_modal.addEventListener('click', () => {
+        modal.style.top = "-270px";
+        container.style.opacity = "1";
+    });
+    
+    document.getElementById('body').addEventListener('click', () => {
+        modal.style.top = "-270px";
+        container.style.opacity = "1";
+    })
+}
+
 const navbar = document.querySelector(".navbar");
 
 const mainContainer = document.querySelector(".main-container");
